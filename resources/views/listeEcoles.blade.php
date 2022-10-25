cc9c2b63d6ae74503b45e22594237b2865a1a60e
Lucas VILLEMAIN <roludellau@gmail.com>
2022-10-25 16:11:14 +0200

    Affichage basique de la liste des écoles de Versailles

--- resources/views/listeEcoles.blade.php
+++ resources/views/listeEcoles.blade.php
@@ -1,12 +1,31 @@
 @include('parts.header')
 
-    <h1>Hello, world!</h1>
-        <ul>
+    <h1 class="titre">Liste des ecoles de Versailles</h1>
+        <div class="containerFlex">
             @foreach ($schools as $school)
-                <li>
-                    {{$school->record->fields->nom_etablissement}}
-                </li>
+                <div class="cardEcole">
+                    <p class="nomEtablissement">
+                        {{$school->record->fields->nom_etablissement}}
+                    </p>
+                    <ul>
+                        <li class="elementListeCard">
+                            <label>Type d'établissement : </label>{{" ".$school->record->fields->type_etablissement}}
+                        </li>
+                        <li class="elementListeCard">
+                            <label>adresse : </label>{{" ".$school->record->fields->adresse_1." ".$school->record->fields->adresse_2}}
+                        </li>
+                        <li class="elementListeCard">
+                            <label>Contact : </label>{{" ".$school->record->fields->mail}}
+                        </li>
+                        <li class="elementListeCard">
+                            <label>Telephone : </label>{{" ".$school->record->fields->telephone}}
+                        </li>
+                        <li class="elementListeCard">
+                            <label>Telephone : </label>{{" ".$school->record->fields->telephone}}
+                        </li>
+                    </ul>
+                </div>
             @endforeach
-        </ul>
+            </div>
 
 @include('parts.footer')
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -6,10 +6,9 @@
     <title>EdukaMaps</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="{{url('css/main.css')}}" rel="stylesheet">
-    {{-- <link href="http://localhost:8000/css/main.css" rel="stylesheet"> --}}
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Playfair+Display+SC&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 
@@ -17,7 +16,7 @@
 
     <header>
         <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-          <a class="navbar-brand" href="#">Carousel</a>
+          <a class="navbar-brand" href="#">Education Versailles</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
