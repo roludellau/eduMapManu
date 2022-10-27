e228f91869839e011fd099b95e7f4d0d87fee196
TheoSoko <you@example.com>
2022-10-27 10:12:34 +0200

    Merging

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -28,11 +28,6 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/liste">Liste</a>
                 </li>
-<<<<<<< HEAD
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="/compte">Mon Compte</a>
-                </li>
-=======
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                       Mon Compte
@@ -45,7 +40,6 @@
                        <a class="dropdown-item" href="#">Profil</a>
                     </div>
                   </li>
->>>>>>> userFormValidation
                 </ul>
             </div>
             </nav>
